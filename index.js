191f2c2e0f7d1e127667bcc2e27af2cf7f56a3f0
Defne Celikoyar <defne.celikoyar@gmail.com>
2021-11-28 17:35:13 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+function batteryCounter(totalAmount, batch) {
+    return totalAmount + batch;
+}
+const firstAttempt = function (arr, reducer, init) {
+    let accum = init;
+    for (const element of arr) {
+        accum = reducer(accum, element);
+    };
+    return accum;
+}
 
-// Code your solution here
+//console.log(totalBatteries(batteryBatches, batteryCounter, 0));
+
+const totalBatteries = [4, 5, 3, 4, 4, 6, 5].reduce(function(accumulator, element){ return element + accumulator;}, 0)
+
+
+// Create a new variable called totalBatteries, which holds the sum of all of the battery amounts in the batteryBatches array.
+//Naturally, you should use reduce() for this!
+//const couponLocations = [
+//    { room: 'Living room', amount: 5 },
+//    { room: 'Master bedroom', amount: 7 }
+//  ];
+
+//  function couponCounter(totalAmount, location) {
+//    return totalAmount + location.amount;
+//  }
+//  console.log(ourReduce(couponLocations, couponCounter, 0)); // LOG: 15
